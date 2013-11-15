ba0ee5b91bf38fa79555cb22b52c045573af1d84
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-11-15 14:55:32 +0900

    refs 5649 modified pankuzu

--- app/controllers/series_statement_relationship_types_controller.rb
+++ app/controllers/series_statement_relationship_types_controller.rb
@@ -1,6 +1,7 @@
 class SeriesStatementRelationshipTypesController < InheritedResources::Base
-  add_breadcrumb "I18n.t('page.listing', :model => I18n.t('activerecord.models.series_statement_relationship_type'))", 'users_path', :only => :index
-  add_breadcrumb "I18n.t('page.editing', :model => I18n.t('activerecord.models.series_statement_relationship_type'))", 'edit_user_path(params[:id])', :only => [:edit, :update]
+  add_breadcrumb "I18n.t('page.showing', :model => I18n.t('activerecord.models.series_statement_relationship_type'))", 'series_statement_relationship_type_path(params[:id])', :only => :show
+  add_breadcrumb "I18n.t('page.listing', :model => I18n.t('activerecord.models.series_statement_relationship_type'))", 'series_statement_relationship_types_path', :only => :index
+  add_breadcrumb "I18n.t('page.editing', :model => I18n.t('activerecord.models.series_statement_relationship_type'))", 'edit_series_statement_relationship_type_path(params[:id])', :only => [:edit, :update]
   load_and_authorize_resource
 
   def index
