e8076a26240b71a870aa1f5ae5cb2aaa7dd96084
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-11-15 12:15:37 +0900

    refs 5649 modified relationship_family column

--- db/migrate/20131022030926_create_relationship_families.rb
+++ db/migrate/20131022030926_create_relationship_families.rb
@@ -1,6 +1,7 @@
 class CreateRelationshipFamilies < ActiveRecord::Migration
   def change
     create_table :relationship_families do |t|
+      t.string :fid
       t.string :display_name, :null => false
       t.text :description
       t.text :note
