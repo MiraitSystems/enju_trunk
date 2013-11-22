d5ac43ddc426a90ec8927c4ffcec1483a8bc47d9
Daisuke HOSHINO <hoshino.daisuke@miraitsystems.jp>
2013-11-22 17:53:04 +0900

    refs 5932 関連タブの表示を仕様書の形に変更

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -191,6 +191,7 @@ class PatronsController < ApplicationController
     if @countalias == 0
       @patron.patron_aliases << PatronAlias.new
     end
+    @papa = PatronAlias.all
     prepare_options
   end
 
