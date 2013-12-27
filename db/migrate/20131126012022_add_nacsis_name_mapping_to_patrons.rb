31710fb223f6e09e9daa6687d39ae8631871b34a
emiko <tamiya.emiko@miraitsystems.jp>
2013-12-27 15:15:43 +0900

    correct migration file and localize label

--- db/migrate/20131126012022_add_nacsis_name_mapping_to_patrons.rb
+++ db/migrate/20131126012022_add_nacsis_name_mapping_to_patrons.rb
@@ -4,8 +4,8 @@ class AddNacsisNameMappingToPatrons < ActiveRecord::Migration
     add_column :patrons, :marcid, :string
     add_column :patrons, :lcaid, :string
 
-    add_index  :patrons, ["source"], :name => "index_patrons_on_source" :unique => false
-    add_index  :patrons, ["marcid"], :name => "index_patrons_on_marcid" :unique => false
-    add_index  :patrons, ["lcaid"],  :name => "index_patrons_on_lcaid"  :unique => true
+    add_index  :patrons, ["source"], :name => "index_patrons_on_source", :unique => false
+    add_index  :patrons, ["marcid"], :name => "index_patrons_on_marcid", :unique => false
+    add_index  :patrons, ["lcaid"],  :name => "index_patrons_on_lcaid",  :unique => true
   end
 end
