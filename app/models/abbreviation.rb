a193de9997866576c8151634e85bdad6e883bb93
Yukimitsu Ogido <ogido.yukimitsu@miraitsystems.jp>
2013-12-27 15:01:56 +0900

    refs 6044 略語作成機能の作成
    略語作成機能の追加

--- app/models/abbreviation.rb
+++ app/models/abbreviation.rb
@@ -1,3 +1,16 @@
 class Abbreviation < ActiveRecord::Base
   attr_accessible :keyword, :v
+
+  def self.get_abbreviation(bookname)
+    merge_v = ""
+    bookname.split(" ").each do |key|
+      @abberviation = Abbreviations.find(key)
+      if merge_v = "" then
+        merge_v = abbreviation.v
+      else
+        merge_v.concat(" ").concat(abbreviation.v)
+      end
+    end
+    return merge_v
+  end
 end
