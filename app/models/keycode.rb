216c3b03852c2764463083ee436ed8088fa3746a
Yukihiro KONDO <youkeyhero@gmail.com>
2014-01-09 14:59:37 +0900

    refs 6106 必須チェック追加、ページング処理追加 etc

--- app/models/keycode.rb
+++ app/models/keycode.rb
@@ -1,4 +1,14 @@
 class Keycode < ActiveRecord::Base
   attr_accessible :display_name, :ended_at, :keyname, :name, :started_at, :v
+  validates_presence_of :name, :display_name, :v, :keyname, :started_at
+  validates_format_of :name, :with => /^[0-9A-Za-z]/ #, :message =>"は半角英数字で入力してください。"
   default_scope :order => 'name ASC, started_at ASC'
+
+  paginates_per 10
+
+  def validate
+    unless self.started_at < self.ended_at
+      errors.add(:base, I18n.t('activerecord.attributes.keycode.ended_at_invalid'))
+    end
+  end
 end
