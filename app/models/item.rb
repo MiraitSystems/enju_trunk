0ec9f3e11b19a424448c57fac9c03d52ed22b41e
emiko <tamiya.emiko@miraitsystems.jp>
2014-01-31 17:27:55 +0900

    manifestation has one approval

--- app/models/item.rb
+++ app/models/item.rb
@@ -1176,17 +1176,16 @@ class Item < ActiveRecord::Base
     raise "invalid parameter: no path" if out_dir.nil? || out_dir.length < 1
     pdf_file = out_dir + "#{type}_catalog.pdf"
     logger.info "output #{type}_catalog  pdf: #{pdf_file}"
-    FileUtils.mkdir_p(out_dir) unless FileTest.exist?(out_dir)
-    if type == 'title'
+FileUtils.mkdir_p(out_dir) unless FileTest.exist?(out_dir)
+if type == 'title'
       @manifestations = Manifestation.order("original_title ASC")
     elsif type == 'author' 
-      @manifestations = Manifestation.find(:all, :include => [:creators], :order => "patrons.full_name asc")
+      @manifestations = Manifestation.includes(:creates => :patron).order("patrons.full_name ASC")
     elsif type == 'classifild'
       @manifestations = Manifestation.order("ndc ASC")
     end
     make_catalog_pdf(pdf_file, @manifestations, "#{type}_catalog") if file_type.nil? || file_type == "pdf"
   end
-
 
   def self.make_catalog_pdf(pdf_file, manifestations, list_title = nil)
     report = ThinReports::Report.new :layout => File.join(Rails.root, 'report', "#{list_title}.tlf")
@@ -1202,22 +1201,24 @@ class Item < ActiveRecord::Base
     report.start_new_page
     report.page.item(:date).value(Time.now)
     report.page.item(:list_name).value(I18n.t("item_register.#{list_title}"))
-    manifestations.find_each do |manifestation|
-      manifestation.items.each do |item|
-        report.page.list(:list).add_row do |row|
-          row.item(:title).value(item.manifestation.original_title) if item.manifestation
-          row.item(:patron).value(item.manifestation.creators[0].full_name) if item.manifestation && item.manifestation.creators[0]
-          row.item(:carrier_type).value(item.manifestation.carrier_type.display_name.localize) if item.manifestation && item.manifestation.carrier_type
+    0.step(manifestations.count, 100) do |offset|
+      manifestations.each do |manifestation|
+        manifestation.items.each do |item|
+          report.page.list(:list).add_row do |row|
+            row.item(:title).value(item.manifestation.original_title) if item.manifestation
+            row.item(:patron).value(item.manifestation.creators[0].full_name) if item.manifestation && item.manifestation.creators[0]
+            row.item(:carrier_type).value(item.manifestation.carrier_type.display_name.localize) if item.manifestation && item.manifestation.carrier_type
 
-          row.item(:library).value(item.shelf.library.display_name.localize) if item.shelf && item.shelf.library
-          row.item(:shelf).value(item.shelf.display_name) if item.shelf
-          row.item(:ndc).value(item.manifestation.ndc) if item.manifestation
-          row.item(:item_identifier).value(item.item_identifier)
-          row.item(:call_number).value(call_numberformat(item))
+            row.item(:library).value(item.shelf.library.display_name.localize) if item.shelf && item.shelf.library
+            row.item(:shelf).value(item.shelf.display_name) if item.shelf
+            row.item(:ndc).value(item.manifestation.ndc) if item.manifestation
+            row.item(:item_identifier).value(item.item_identifier) if item.item_identifier
+            row.item(:call_number).value(call_numberformat(item)) if call_numberformat(item)
+          end
         end
       end
+      report.generate_file(pdf_file)
     end
-    report.generate_file(pdf_file)
   end
 
   def self.make_export_item_list_job(file_name, file_type, method, dumped_query, args, user)
@@ -1329,8 +1330,8 @@ class Item < ActiveRecord::Base
       message(
         user,
         I18n.t('item_register.export_job_error_subject', :job_name => job_name),
-        I18n.t('item_register.export_job_error_body', :job_name => job_name, :message => exception.message),
-      )
+        I18n.t('item_register.export_job_error_body', :job_name => job_name, :message => exception.message)
+        )
     end
   end
 end
