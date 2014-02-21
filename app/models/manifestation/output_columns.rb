d101715be6d5dc79c6827736a1d76b99ae10c0d0
hoshinodis <hoshino.daisuke@miraitsystems.jp>
2014-02-21 16:50:42 +0900

    refs6599 updated output_columns.rb

--- app/models/manifestation/output_columns.rb
+++ app/models/manifestation/output_columns.rb
@@ -5,13 +5,25 @@ module Manifestation::OutputColumns
     isbn original_title title_transcription title_alternative carrier_type jpn_or_foreign
     frequency pub_date country_of_publication place_of_publication language
     edition_display_value volume_number_string issue_number_string serial_number_string lccn
-    marc_number ndc start_page end_page height width depth price
+    marc_number ndc start_page end_page size price
     acceptance_number access_address repository_content required_role
-    except_recent description supplement note creator contributor publisher
+    except_recent description supplement note 
+    manifestation_extext.note1 manifestation_extext.note2 
+    manifestation_exinfo.author_mark 
+    creator contributor publisher
     subject accept_type acquired_at_string bookstore library shelf checkout_type
     circulation_status retention_period call_number item_price url
     include_supplements use_restriction item_note rank item_identifier
-    remove_reason non_searchable missing_issue del_flg
+    remove_reason non_searchable missing_issue 
+    manifestation_exinfo.spare1 manifestation_exinfo.spare2 manifestation_exinfo.spare3 
+    manifestation_exinfo.spare4 manifestation_exinfo.spare5 manifestation_exinfo.spare6 
+    manifestation_exinfo.spare7 manifestation_exinfo.spare8 manifestation_exinfo.spare9 
+    manifestation_exinfo.spare10 
+    manifestation_extext.detail1 manifestation_extext.detail2 manifestation_extext.detail3 
+    manifestation_extext.detail4 manifestation_extext.detail5 manifestation_extext.detail6 
+    manifestation_extext.detail7 manifestation_extext.detail8 manifestation_extext.detail9 
+    manifestation_extext.detail10 manifestation_extext.detail11 manifestation_extext.detail12
+    del_flg
   ).map{ |c| c unless  c == '' }.compact }
   SERIES_COLUMNS = %w(
     issn original_title title_transcription periodical
