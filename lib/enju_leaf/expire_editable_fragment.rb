d4c6d3cf9bad92cd78e85112af84bfc465d12722
Akifumi NAKAMURA <nakamura.akifumi@miraitsystems.jp>
2014-03-12 17:40:47 +0900

    refs 6754 expire_fragmentの処理を修正。

--- lib/enju_leaf/expire_editable_fragment.rb
+++ lib/enju_leaf/expire_editable_fragment.rb
@@ -1,31 +1,30 @@
 module ExpireEditableFragment
-  def expire_editable_fragment(record, fragments = [])
-    fragments.uniq!
-    if record.is_a?(Manifestation)
-      if fragments.empty?
-        #fragments = %w[manifestation_html show_detail_user_html show_detail_librarian_html pickup_html title_html title_mobile index_sru list_identifiers_oai list_records_oai]
-        fragments = %w[manifestation_html show_detail_user_html show_detail_librarian_html pickup_html title_html]
-      end
-      expire_manifestation_cache(record, fragments)
-    else
-      I18n.available_locales.each do |locale|
-        Role.all_cache.each do |role|
-          fragments.each do |fragment|
-            expire_fragment(":#{record.class.to_s.downcase}" => record.id, :fragment => fragment, :role => role.name, :locale => locale)
-          end
-        end
-      end
-    end
-  end
+	def expire_editable_fragment(record, fragments = [])
+		fragments.uniq!
+		if record.is_a?(Manifestation)
+			if fragments.empty?
+				fragments = %w[detail pickup book_jacket title picture_file show_list edit_list reserve_list show_index] 
+			end
+			expire_manifestation_cache(record, fragments)
+		else
+			I18n.available_locales.each do |locale|
+				Role.all_cache.each do |role|
+					fragments.each do |fragment|
+						expire_fragment(:controller => record.class.to_s.pluralize.downcase, :action => :show, :id => record.id, :page => fragment, :role => role.name, :locale => locale)
+					end
+				end
+			end
+		end
+	end
 
-  private
-  def expire_manifestation_cache(manifestation, fragments = [])
-    I18n.available_locales.each do |locale|
-      Role.all_cache.each do |role|
-        fragments.uniq.each do |fragment|
-          expire_fragment([:manifestation => manifestation.id, :fragment => fragment, :role => role.name, :locale => locale])
-        end
-      end
-    end
-  end
+	private
+	def expire_manifestation_cache(manifestation, fragments = [])
+		I18n.available_locales.each do |locale|
+			Role.all_cache.each do |role|
+				fragments.uniq.each do |fragment|
+					expire_fragment(:controller => :manifestations, :action => :show, :id => manifestation.id, :page => fragment, :role => role.name, :locale => locale, :manifestation_id => nil)
+				end
+			end
+		end
+	end
 end
