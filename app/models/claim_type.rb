36a31e415ecf32dda0312fa0d1f6d8dbfff304ea
morimoto002 <morimoto.tatsuya002@miraitsystems.jp>
2014-03-12 11:26:25 +0900

    refs 6732 new に制約の追加

--- app/models/claim_type.rb
+++ app/models/claim_type.rb
@@ -2,6 +2,7 @@ class ClaimType < ActiveRecord::Base
   has_many :claims, :dependent => :destroy
 
   attr_accessible :display_name, :name
-
+ 
+  validates_presence_of :name
   paginates_per 10
 end
