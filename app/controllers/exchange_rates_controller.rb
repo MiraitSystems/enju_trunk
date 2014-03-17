466cd53fb910f58c888f761ccfe50fd48ff15bcc
emiko <tamiya.emiko@miraitsystems.jp>
2014-03-17 20:03:35 +0900

    refs 6553 update Order#exchange_rate

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 class ExchangeRatesController < ApplicationController
   add_breadcrumb "I18n.t('page.listing', :model => I18n.t('activerecord.models.exchange_rate'))", 'exchange_rates_path', :only => [:index]
   add_breadcrumb "I18n.t('page.showing', :model => I18n.t('activerecord.models.exchange_rate'))", 'exchange_rate_path(params[:id])', :only => [:show]
@@ -6,18 +7,45 @@ class ExchangeRatesController < ApplicationController
 
   before_filter :check_client_ip_address
   load_and_authorize_resource
+  after_filter :solr_commit, :only => [:create, :update, :destroy]
 
   def index
 
-    if params[:currency] != nil && params[:currency][:display_name] != ''
-      @exchange_rates = ExchangeRate.where(["currency_id = ?",params[:currency][:display_name]]).order("started_at desc").page(params[:page])
-      @currencies_selected = params[:currency][:display_name]
-    else
-      @exchange_rates = ExchangeRate.order("started_at desc").page(params[:page])
-    end	
+    @count = {}
+    page = params[:page] || 1
+    per_page = params[:format] == 'tsv' ? 65534 : ExchangeRate.default_per_page
+    query = params[:query].to_s.strip
 
-    @currencies = Currency.find(:all, :order => "display_name")
+    order_list = @order_list
+    search = ExchangeRate.search.build do
+      fulltext query if query
+      order_by(:started_at, :desc)
+      paginate :page => page.to_i, :per_page => per_page
+    end.execute
+    @exchange_rates = search.results
+    @currencies = Currency.find(:all, :order => "display_name DESC")
 
+    # 発注への通貨レート反映処理
+    begin
+      start_at = params[:orders_started_at]
+      end_at = params[:orders_ended_at]
+      update_to_orders = params[:update_to_orders]
+
+      if update_to_orders
+        ExchangeRate.check_order_date(start_at, end_at)
+        orders = Order.where(:ordered_at => start_at...end_at)
+        orders.each do |order|
+          newest_rate = ExchangeRate.find(:first, :conditions => { :currency_id => (order.currency_id) }, :order => "started_at DESC")
+          order.currency_rate = newest_rate.rate
+          order.save
+        end
+        flash[:orders_notice] = t('controller.successfully_updated', :model => t('activerecord.models.order'))
+        redirect_to :action => "index"
+      end
+    rescue => e
+        flash[:orders_error] = e.message
+        redirect_to :action => "index"
+    end 
   end
 
   def new
--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,12 +1,39 @@
 class ExchangeRate < ActiveRecord::Base
-  attr_accessible :created_at, :currency_id, :id, :rate, :started_at, :updated_at
+  attr_accessible :created_at, :currency_id, :id, :rate, :started_at, :updated_at, :orders_started_at, :orders_ended_at
+  attr_accessor :orders_started_at, :orders_ended_at
+
   belongs_to :currency
 
   paginates_per 10
 
+  validates_uniqueness_of :currency_id
   validates :rate, :presence => true
   validates :started_at, :presence => I18n.t('exchange_rate.invalid_started_at')
 
   validates_numericality_of :rate, :less_than => 100000000
+
+  searchable do
+    text :currency_display_name do
+      currency.try(:display_name)
+    end
+    string :currency_id
+    time :started_at
+  end
+
+  def self.check_order_date(start_at, end_at)
+    if start_at.present? && end_at.present?
+      begin
+        start_at_new = Time.zone.parse(start_at)
+        end_at_new = Time.zone.parse(end_at)
+      rescue ArgumentError
+        raise I18n.t('activerecord.attributes.order.ordered_at')+I18n.t('exchange_rate.invalid_started_at')
+      end
+      if start_at > end_at
+        raise I18n.t('activerecord.attributes.order.ordered_at')+I18n.t('exchange_rate.invalid_started_at')
+      end
+    else
+      raise I18n.t('activerecord.attributes.order.ordered_at')+I18n.t('exchange_rate.invalid_started_at')
+    end
+  end
 
 end
