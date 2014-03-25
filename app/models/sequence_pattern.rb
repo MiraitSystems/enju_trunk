5d248b0b3e5640a0737ec8a7b75a0c906cf36e79
emiko <tamiya.emiko@miraitsystems.jp>
2014-03-25 15:53:39 +0900

    refs 6859 get next volume and issue number

--- app/models/sequence_pattern.rb
+++ app/models/sequence_pattern.rb
@@ -5,18 +5,22 @@ class SequencePattern < ActiveRecord::Base
 
   def get_next_number(current_volume, current_issue)
     next_volume, next_issue = nil, nil
-    next_issue = current_issue + self.issue_param if self.issue_param
-    if next_issue >= self.volume_param
-      next_issue = 1 if self.issue_param
-      next_volume = current_volume + 1 if self.volume_param
-    end
+    next_issue = current_issue + self.issue_param if current_issue && self.issue_param
+    unless next_issue.nil?
+      if self.volume_param && next_issue > self.volume_param * self.issue_param
+        next_issue = 1 if self.issue_param
+        next_volume = current_volume + 1 if current_volume && self.volume_param
+      else
+        next_volume = current_volume
+      end
 
-    case self.sequence_type
-    when 0 # cardinal
-    when 1 # ordinal
-      next_issue = next_issue.ordinalize 
-    when 2 # month
-      next_issue = Date::ABBR_MONTHNAMES[next_issue]
+      case self.sequence_type
+      when 0 # cardinal
+      when 1 # ordinal
+        next_issue = next_issue.ordinalize 
+      when 2 # month
+        next_issue = Date::ABBR_MONTHNAMES[next_issue]
+      end
     end
     return [next_volume, next_issue]
   end
