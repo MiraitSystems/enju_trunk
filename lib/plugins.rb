ccdc7948c38292a5a06f477f31ffc0e9287b9e0e
akira yamada <akira@arika.org>
2014-04-18 10:48:50 +0900

    enju_trunk_circulationなしでも動作するようにした
    enju_trunk_circulationのrequireにrescue nilが付与されているのはロードできない場合への対処であると推測し、他のenju_trunk_ill、enju_trunk_subjectと同様にenju_trunk_circulationをロードできなかったときには無視するようにした。

--- lib/plugins.rb
+++ lib/plugins.rb
@@ -1,15 +1,21 @@
 # http://railspress.matake.jp/extend-plugin-gem-library-in-rails-project
 require File.expand_path(File.join(File.dirname(__FILE__), 'plugins', 'ext'))
 
-require 'enju_trunk_circulation' rescue nil
+begin
+  require 'enju_trunk_circulation'
+rescue LoadError
+  warn $!
+end
+
 begin
   require 'enju_trunk_ill' 
 rescue LoadError
-  puts "info: enju_trunk_ill error"
+  warn $!
 end
+
 begin
   require 'enju_trunk_statistics' 
 rescue LoadError
-  puts "info: enju_trunk_statistics"
+  warn $!
 end
  
