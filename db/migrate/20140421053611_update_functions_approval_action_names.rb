19fb9648d7a30993735e3d533ee3110ed99c8d03
mtatsuya <tatsuya_murayama@kme.biglobe.ne.jp>
2014-04-21 15:27:08 +0900

    見計らいのアクションを追加

---
+++ db/migrate/20140421053611_update_functions_approval_action_names.rb
@@ -0,0 +1,11 @@
+class UpdateFunctionsApprovalActionNames < ActiveRecord::Migration
+  def up
+    function = Function.where("controller_name = 'ApprovalsController'").first
+    function.update_attribute(:action_names, "read:index,show,search,output_csv\nupdate:new,create,edit,update,get_approval_report\ndelete:destroy\n")
+  end
+
+  def down
+    function = Function.where("controller_name = 'ApprovalsController'").first
+    function.update_attribute(:action_names, "read:index,show\nupdate:new,create,edit,update,get_approval_report\ndelete:destroy\n")
+  end
+end
