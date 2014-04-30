32ea27b17569ff299287a18bef00afa56c1e5225
yogoyasuo <yogo@se-family.jp>
2014-04-30 19:10:39 +0900

    ref 7060 SubCarrierTypeの作成
    1. システムの設定 > 形態グループに CRUD を作成
    2. Manifestation#sub_carrier_type_id を追加し、Manifestation の new/edit
    画面で登録
    3. SeriesStatement の RootManifestation にも対応
    4. fixtures を作成

--- app/models/sub_carrier_type.rb
+++ app/models/sub_carrier_type.rb
@@ -7,8 +7,6 @@ class SubCarrierType < ActiveRecord::Base
 
   attr_accessible :carrier_type_id, :display_name, :nacsis_identifier, :name, :note, :position
 
-  validates_presence_of :name
   validates_presence_of :display_name
-  validates_presence_of :nacsis_identifier
 
 end
