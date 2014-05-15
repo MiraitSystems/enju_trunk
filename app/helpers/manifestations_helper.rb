94499fd2c22a41b40a03f2e9cfd376b66d267629
asoda <asoda.masataka@miraitsystems.jp>
2014-05-15 15:19:32 +0900

    検索結果から定期刊行物表示した際の[戻る]リンク追加

--- app/helpers/manifestations_helper.rb
+++ app/helpers/manifestations_helper.rb
@@ -11,9 +11,10 @@ module ManifestationsHelper
   end
 
   def back_to_manifestation_index
+    logger.error "######## session = #{session[:params]} ########"
     if session[:params]
       session[:params][:output_pdf], session[:params][:output_tsv], session[:params][:output_excelx], session[:params][:output_request] = nil,nil,nil,nil
-      params = session[:params].merge(:view => nil, :controller => :manifestations)
+        params = session[:params].merge(:view => nil, :series_statement_id => nil, :controller => :manifestations)
       link_to t('page.back_to_search_results'), url_for(params)
     else
       link_to t('page.back'), :back
