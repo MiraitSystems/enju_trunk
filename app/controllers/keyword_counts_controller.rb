e1ae03565114d36dfef5c7f137e50ecf7df23fb7
asoda <asoda.masataka@miraitsystems.jp>
2014-05-21 16:27:33 +0900

    検索履歴の初期記録場所をDBに設定

--- app/controllers/keyword_counts_controller.rb
+++ app/controllers/keyword_counts_controller.rb
@@ -3,6 +3,7 @@ class KeywordCountsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    # KeywordCount.calc()
     @start_d = params[:start_d] ? params[:start_d] : (Date.today - 1.month).strftime("%Y-%m-%d")
     @end_d = params[:end_d] ? params[:end_d] : Date.today.strftime("%Y-%m-%d")
     @keyword_counts, flash[:message] = KeywordCount.create_ranks(@start_d, @end_d)
