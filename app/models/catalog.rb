13dc1db7898da2200daf5790de9470df7db940c0
yogoyasuo <yogo@se-family.jp>
2014-05-28 19:23:46 +0900

    refs 7001 NACSIS CAT: Catalog CRUD, 7060 SubCarrierTypeの作成
    指摘事項の修正

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -3,7 +3,5 @@ class Catalog < ActiveRecord::Base
   attr_accessible :display_name, :nacsis_identifier, :name, :note
 
   validates_presence_of :name
-  validates_presence_of :nacsis_identifier
   validates_uniqueness_of :name
-  validates_uniqueness_of :nacsis_identifier
 end
