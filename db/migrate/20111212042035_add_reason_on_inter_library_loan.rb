c64332c0350db40e6be93fc5fbdfdd23e9998e48
emiko <tamiya.emiko@miraitsystems.jp>
2014-06-13 19:06:28 +0900

    refs 7500 gem 化した機能migratin fileはgem内に配置

--- db/migrate/20111212042035_add_reason_on_inter_library_loan.rb
+++
@@ -1,9 +0,0 @@
-class AddReasonOnInterLibraryLoan < ActiveRecord::Migration
-  def self.up
-    add_column :inter_library_loans, :reason, :integer
-  end
-
-  def self.down
-    remove_column :inter_library_loans, :reason
-  end
-end
--- db/migrate/20131007084751_add_checked_at_on_checkins.rb
+++
@@ -1,9 +0,0 @@
-class AddCheckedAtOnCheckins < ActiveRecord::Migration
-  def up
-    add_column :checkins, :checked_at, :datetime
-  end
-
-  def down
-   remove_column :checkins, :checked_at
-  end
-end
