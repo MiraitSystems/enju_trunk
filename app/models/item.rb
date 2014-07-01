160f9e3190879151249ca1962adbe6125f051d08
asoda <asoda.masataka@miraitsystems.jp>
2014-07-01 16:22:31 +0900

    itemへtax_rate_idカラムの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ActiveRecord::Base
                   :shelf_id, :circulation_status, :bookstore, :remove_reason, :checkout_type, 
                   :shelf, :bookstore, :retention_period, :accept_type_id, :accept_type, :required_role,
                   :non_searchable, :item_has_operators_attributes,
-                  :non_searchable, :item_exinfo, :claim_attributes, :payment_id, :location_category_id, :location_symbol_id, :statistical_class_id
+                  :non_searchable, :item_exinfo, :claim_attributes, :payment_id, :location_category_id, :location_symbol_id, :statistical_class_id, :tax_rate_id
 
   self.extend ItemsHelper
   scope :sort_rank, order('rank')
@@ -87,6 +87,7 @@ class Item < ActiveRecord::Base
   belongs_to :location_symbol, :class_name => 'Keycode', :foreign_key => 'location_symbol_id'
   belongs_to :location_category, :class_name => 'Keycode', :foreign_key => 'location_category_id'
   belongs_to :statistical_class, :class_name => 'Keycode', :foreign_key => 'statistical_class_id'
+  belongs_to :tax_rate, :class_name => 'TaxRate', :foreign_key => 'tax_rate_id'
 
   validates_associated :circulation_status, :shelf, :bookstore, :checkout_type, :retention_period
   # validates_associated :exemplify, :message => I18n.t('import_request.isbn_taken')
