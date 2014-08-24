b555fc4e78743b54eb690f5f1078e9d46922298b
Akifumi NAKAMURA <nakamura.akifumi@miraitsystems.jp>
2014-08-24 14:49:47 +0900

    refs 8222 延滞罰則機能（遅延日数の算出）

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,4 +1,20 @@
 require EnjuTrunkCirculation::Engine.root.join('app', 'models', 'checkout')
 class Checkout < ActiveRecord::Base
     attr_accessible :librarian_id, :item_id, :basket_id, :due_date, :created_at
+
+    def user_group_has_checkout_type
+      UserGroupHasCheckoutType.where(user_group_id: user.user_group_id, checkout_type_id: item.checkout_type_id).first
+    end
+
+    def days_overdue
+      pp user_group_has_checkout_type
+      number_of_delay = user_group_has_checkout_type.days_overdue
+      due_date = self.due_date.localtime.to_date
+      diff = Date.today - (due_date + number_of_delay.days)
+      diff_i = diff.to_i
+      if diff_i < 0
+        diff_i = 0
+      end
+      return diff_i
+    end
 end
