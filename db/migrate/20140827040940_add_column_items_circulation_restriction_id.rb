2ff3b1c64ba02bc53421ff543876c821da8a2827
emiko <tamiya.emiko@miraitsystems.jp>
2014-08-27 15:07:05 +0900

    所蔵に禁帯出管理用カラム追加(migration file)

---
+++ db/migrate/20140827040940_add_column_items_circulation_restriction_id.rb
@@ -0,0 +1,10 @@
+class AddColumnItemsCirculationRestrictionId < ActiveRecord::Migration
+  def change
+    keycode = Keycode.where(:name => 'item.circulation_restriction', :v => '0').try(:first) || 
+              Keycode.create!(:name => 'item.circulation_restriction', :display_name => '資料状況', :v => '0', :keyname => '帯出可', :started_at => '1900/1/1')
+    add_column :items, :circulation_restriction_id, :integer
+    Item.update_all("circulation_restriction_id = #{keycode.id}")
+    change_column :items, :circulation_restriction_id, :integer, :null => false, :default => keycode.id
+    puts "このあとに keycodes fixture をロードすると Keycode.where(:name => 'item.circulation_restriction', :v => '0', :keyname => 帯出可) が重複する可能性があります。確認してください。"
+  end
+end
