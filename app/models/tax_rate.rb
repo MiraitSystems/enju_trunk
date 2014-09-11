bf60f80d92222587ceb1a83352f091beac090313
A5mstk <asoda.masataka@miraitsystems.jp>
2014-09-11 11:15:22 +0900

    所蔵の価格における端数の処理追加

--- app/models/tax_rate.rb
+++ app/models/tax_rate.rb
@@ -9,4 +9,23 @@ class TaxRate < ActiveRecord::Base
   validates :rounding, presence: true
 
   ROUNDING_TYPES = {0 => "切り捨て", 1 => "四捨五入", 2 => "切り上げ"}
+
+  def self.build_tax(date, price)
+    tax_rate = TaxRate.where(["start_date <= ? and end_date >= ?", date, date]).first
+    tax = price * tax_rate.rate * 0.01
+    if (tax_rate.rounding == 0)
+      #切り捨て
+      tax = tax.truncate
+    elsif (tax_rate.rounding == 1)
+      #四捨五入
+      tax = tax.round
+    elsif (tax_rate.rounding == 2)
+      #切り上げ
+      tax = tax.ceil
+    end
+    item_price = price + tax
+    excluding_tax = price
+    tax_rate_id = tax_rate.id
+    return [item_price, excluding_tax, tax, tax_rate_id]
+  end
 end
