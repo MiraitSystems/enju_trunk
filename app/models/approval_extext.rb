19948b96b25970fcc143aa232e020d75906735d7
SHINOZUKA Reiko <shinozuka.reiko@miraitsystems.jp>
2014-09-19 11:44:11 +0900

    modified approval

--- app/models/approval_extext.rb
+++ app/models/approval_extext.rb
@@ -1,7 +1,10 @@
 class ApprovalExtext < ActiveRecord::Base
-  attr_accessible :approval_id, :created_at, :position, :updated_at, :value, :created_by, :state, :comment_at
+  attr_accessible :approval_id, :created_at, :position, :updated_at, :value, :created_by, :state, :comment_at, :name
 
   belongs_to :approval
   belongs_to :create_user, :class_name => "User", :foreign_key => :created_by
 
+  acts_as_list :scope => [:approval_id, :name]
+  default_scope :order => "position ASC"
+  has_paper_trail
 end
