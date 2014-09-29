e35656d03cf9be67d9183de0cf9a4a6dc1cb3860
A5mstk <asoda.masataka@miraitsystems.jp>
2014-09-29 17:11:32 +0900

    スマホ版の遷移できないバグ修正

--- app/assets/javascripts/enju_trunk_mobile.js
+++ app/assets/javascripts/enju_trunk_mobile.js
@@ -1,3 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require portlets
+//= require jquery.simplecalendarjp
+//= require jquery.colorbox-min
+//= require function_key_control
 //= require jquery.mobile
