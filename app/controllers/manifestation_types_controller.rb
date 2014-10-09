ff5a794aa57e0eb25fa5b436a2736050a2104969
emiko <tamiya.emiko@miraitsystems.jp>
2014-10-09 11:30:12 +0900

    refs 8542 ManifestationType にアイコンを登録

--- app/controllers/manifestation_types_controller.rb
+++ app/controllers/manifestation_types_controller.rb
@@ -2,6 +2,7 @@ class ManifestationTypesController < InheritedResources::Base
   respond_to :html, :json
   before_filter :check_client_ip_address
   load_and_authorize_resource
+  before_filter :prepare_options, :only => [:new, :edit]
 
   def update
     @manifestation_types = ManifestationType.find(params[:id])
@@ -11,4 +12,11 @@ class ManifestationTypesController < InheritedResources::Base
     end
     update!
   end
+
+  def prepare_options
+    spec = Gem::Specification.find_by_name("enju_trunk")
+    gem_root = spec.gem_dir
+    files = Dir.glob("#{gem_root}/app/assets/images/icons/manifestation_type_*.png")
+    @icon_files = files.inject([]) {|a, file| a << File.basename(file)} 
+  end 
 end
