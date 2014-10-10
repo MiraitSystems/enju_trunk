d1a675d3e501ff21a459e3d00231ac8dfde3bcec
SHINOZUKA Reiko <shinozuka.reiko@miraitsystems.jp>
2014-10-10 16:48:08 +0900

    modified default scope of approval_exinfos

--- app/models/approval_exinfo.rb
+++ app/models/approval_exinfo.rb
@@ -2,7 +2,7 @@ class ApprovalExinfo < ActiveRecord::Base
   attr_accessible :approval_id, :name, :value, :position 
 
   acts_as_list :scope => [:approval_id, :name]
-  default_scope :ord
+  default_scope order: "position"
   belongs_to :approval
 
   has_paper_trail
