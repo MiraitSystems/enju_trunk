30af341c5fe38f17bb3dec3821cde9856b700df4
SHINOZUKA Reiko <shinozuka.reiko@miraitsystems.jp>
2014-10-10 16:35:23 +0900

    refs 8827 add ApprovalExinfo model

---
+++ app/models/approval_exinfo.rb
@@ -0,0 +1,9 @@
+class ApprovalExinfo < ActiveRecord::Base
+  attr_accessible :approval_id, :name, :value, :position 
+
+  acts_as_list :scope => [:approval_id, :name]
+  default_scope :ord
+  belongs_to :approval
+
+  has_paper_trail
+end
