bc082ed19931ff37909206f14064071903ea6458
SHINOZUKA Reiko <shinozuka.reiko@miraitsystems.jp>
2014-10-16 22:14:36 +0900

    refs 8446 add breadcrumb to sequence patterns

--- app/controllers/sequence_patterns_controller.rb
+++ app/controllers/sequence_patterns_controller.rb
@@ -1,4 +1,8 @@
 class SequencePatternsController < ApplicationController
+  add_breadcrumb "I18n.t('page.listing', :model => I18n.t('activerecord.models.sequence_pattern'))", 'sequence_patterns_path', only: [:index]
+  add_breadcrumb "I18n.t('page.new', :model => I18n.t('activerecord.models.sequence_pattern'))", 'new_sequence_pattern_path', only: [:new, :create]
+  add_breadcrumb "I18n.t('page.editing', :model => I18n.t('activerecord.models.sequence_pattern'))", 'edit_sequence_pattern_path(params[:id])', only: [:edit, :update]
+
   load_and_authorize_resource  
   before_filter :prepare_options, :except => [:index, :destroy]
 
