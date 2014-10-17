150c33bcbb00b5d9dd9616d122e22c4256ab35e5
emiko <tamiya.emiko@miraitsystems.jp>
2014-10-17 13:18:33 +0900

    refs 7434 スマホ版の詳細検索画面

--- app/assets/javascripts/enju_trunk_mobile.js
+++ app/assets/javascripts/enju_trunk_mobile.js
@@ -1,3 +1,4 @@
+//= require enju_trunk
 //= require jquery
 //= require jquery_ujs
 //= require portlets
@@ -5,3 +6,9 @@
 //= require jquery.colorbox-min
 //= require function_key_control
 //= require jquery.mobile
+
+$(document).ready(function(){
+  $("input[id ^= 'except']").parent('div').addClass('except_text_field');
+  $('.date_text_field').parent('div').addClass('date_text_field');
+  $('.num_text_field').parent('div').addClass('num_text_field');
+}); 
